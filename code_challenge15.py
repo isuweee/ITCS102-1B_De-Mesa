619e0b35daea6f269717e78660104111480bad49
De Mesa, Adrian <acesuueee@gmail.com>
2025-11-19 15:50:36 +0800

    code challenge 15

--- code_challenge15.py
+++ code_challenge15.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 os.system('cls')
 print("======================================")
@@ -6,6 +7,7 @@ print("===== STUDENT INFORMATION SYSTEM =====")
 print("======================================\n")
 
 record = {}
+# OPTIONS
 while True:
     print("===== OPTIONS =====")
     print("A = Add student record.")
@@ -19,6 +21,7 @@ while True:
 
     pick = input("SELECT OPTION FROM ABOVE: ").lower()
 
+# ADDING STUDENT RECORD
     if pick == 'a':
         print("\n=== ADDING STUDENT RECORD ===\n")
         id = input("Student ID: ").upper()
@@ -30,25 +33,108 @@ while True:
 
         record[id] = [fn, ln, age, section, course]
         print("\n======== DATA SAVED ========\n")
-
         continue
+
+# PRINTING STUDENT RECORD
     elif pick == 'b':
         print("===== STUDENT RECORD =====")
         for a, d in record.items():
-            print(f'CODE - {a}, INFORMATION - {d}')
+            print(f'STUDENT ID: {a}, INFORMATION - {d}')
         continue
+
+# SEARCHING STUDENT RECORD
     elif pick == 'c':
-        pass
+        os.system('cls')
+        print("=== SEARCH STUDENT RECORD ===")
+        search = input("STUDENT ID NO: ")
+
+        for each_id in record:
+            if each_id in record.keys():
+                print("=====RECORD FOUND=====")
+                print(f'RECORD FOR: {search}')
+                
+                for a in record[search]:
+                    print(f"- {a}")
+                print("======================")
+            else:
+                print("NO RECORD FOUND")
+            break
         continue
+
+# DELETE STUDENT RECORD
     elif pick == 'd':
-        pass
+        os.system('cls')
+        print("=== SEARCH STUDENT RECORD ===")
+        search = input("STUDENT ID NO: ")
+
+        for each_id in record:
+            if each_id in record.keys():
+                print("=====RECORD FOUND=====")
+                print(f'RECORD FOR: {search}')
+                
+                for a in record[search]:
+                    print(f"- {a}")
+                print("======================")
+                
+                confirm = input("CONFIRM DELETE (Y/N): ").lower()
+                if confirm == 'y':
+                    record.pop(search)
+                    print("RECORD DELETED")
+                    break
+                elif confirm == 'n':
+                    print("NOT DELETED")
+                    break
+                else:
+                    print("Invalid Option")
+                    continue
+            else:
+                print("NO RECORD FOUND")
+            break
         continue
+# EDIT STUDENT RECORD
     elif pick == 'e':
-        pass
+        os.system('cls')
+        print("=== SEARCH STUDENT RECORD ===")
+        search = input("STUDENT ID NO: ")
+
+        for each_id in record:
+            if each_id in record.keys():
+                print("=====RECORD FOUND=====")
+                print(f'RECORD FOR: {search}')
+                
+                for a in record[search]:
+                    print(f"- {a}")
+                print("======================")
+
+                # NEW SET OF VALUES
+                fn = input("First Name: ").upper()
+                ln = input("Last Name: ").upper()
+                age = eval(input("Age: "))
+                section = input("Section: ").upper()
+                course = input("Course: ").upper()
+
+                record[search][0] = fn
+                record[search][1] = ln
+                record[search][2] = age
+                record[search][3] = section
+                record[search][4] = course
+
+                print("DATA IS NOW UPDATED")
+                
+            else:
+                print("NO RECORD FOUND")
+            break
         continue
+
+# EXPORT STUDENT RECORD
     elif pick == 'f':
-        pass
+        print("===== EXPORT RECORD =====")
+        
+        with open('students_record.json', 'w') as new_file:
+            json.dump(record, new_file, indent=4)
         continue
+
+# EXIT
     elif pick == 'g':
         print("SYSTEM EXIT")
         break

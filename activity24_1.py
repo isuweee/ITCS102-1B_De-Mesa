920abb5499ce4a51c948178c9b609d73f51572fc
De Mesa, Adrian <acesuueee@gmail.com>
2025-11-24 07:12:09 +0800

    DEF Variables for activity 24

---
+++ activity24_1.py
@@ -0,0 +1,7 @@
+# Simple use of DEFINE by De Mesa, Adrian ^^
+
+def cont(cont):
+    print(f'Are you okay? (y/n): {cont}')
+
+def info(name, loc, age):
+    print(f'Hello {name}, I see that you are {age} years old and you live in {loc}. Nice to meet you!')
